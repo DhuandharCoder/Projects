a640e9b0a8cfa14a1aa67e981a4d6c1913b51226
DhuandharCoder <pt4597591@gmail.com>
2026-09-04 11:04:11 +0530

    .

---
+++ Projects/CS50_final.py
@@ -0,0 +1,49 @@
+tasks = []
+def show():
+    for number, task in enumerate(tasks, 1):
+        print(f"{number}. {task}")
+    
+
+def add():
+
+    t = input("> ")
+    t = t.lower()
+    if t.startswith("add"):
+        a, b = t.split("add", 1)
+        if b.strip() == "":
+            return "please enter some task"
+        tasks.append(b)
+        return f"Added: {b.strip()}"
+
+
+    elif t.startswith("a_"):
+        x, y = t.split("a_", 1)
+        if y.strip() == "":
+            return "please enter some task"
+        tasks.append(y)
+        return f"Added: {y.strip()}" 
+    elif t.startswith("show"):
+        show()
+        return ""
+
+    else:
+        return "Invalid Input"
+
+
+        
+
+
+def main():
+    try:
+        while True:
+            print(add())
+    except KeyboardInterrupt:
+        print("\nHave a nice day!")
+    
+    
+    
+if __name__ == "__main__":
+    main()
+
+
+   
---
+++ gui.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Expense Tracker")
+window.geometry("500x500")
+
+window.mainloop()
